a9b0cb18046fb830348e63c7d644c810b95e4d4f
Beth Spencer <beth.spencer@student.labette.edu>
2020-10-28 20:35:23 -0500

    updated css

--- script.js
+++ script.js
@@ -57,14 +57,14 @@ function displayGameInfo(game) {
   $("#game-Title").attr("style", "visibility: visible");
   $("#screenshot").attr("style", "visibility: visible");
   $("<div>")
-    .html("<h3>" + game.results[0].name+"</h3>")
+    .html("<h1>" + game.results[0].name+"</h1>")
     .appendTo("#game-Title");
   $("<img>")
     .attr("src", game.results[0].background_image)
     .css("width", "30%", "height", "30%")
     .appendTo("#screenshot");
   $("<li>")
-    .text("Rating: " + game.results[0].score)
+    .html("<h2>Rating: " + game.results[0].score+"</h2>")
     .appendTo("#game-Info");
   $("<li>")
     .text("Release Date: " + game.results[0].released)
